aa57e944d4dccfebb131ee5216e7f25a64efa786
Wasana Karunasena <141900162+WasanaKarunasena@users.noreply.github.com>
2025-03-12 18:36:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import Signup from './component/common/header/Signup';
 import Home from './component/home/Home';
 import Team from './component/pages/team/Team';
 import Footer from './component/common/footer/Footer';
+import ContactUs from './component/pages/contactus/ContactUs';
+
+
 
 function App() {
   return (
@@ -23,6 +26,7 @@ function App() {
           <Route path="/login" exact component={Login} />
           <Route path="/signup" exact component={Signup} />
           <Route path="/team" exact component={Team} />
+          <Route path="/contactus" exact component={ContactUs} />
           {/* <Route path="/faq" exact component={Faq} /> */}
         </Switch>
         <Footer />
