89bb7a0697397ddca9a2f96577f0fd3d2bd8f94e
Wasana Karunasena <141900162+WasanaKarunasena@users.noreply.github.com>
2025-03-17 21:02:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import {
   //Link
 } from "react-router-dom";
 import Header from './component/common/header/Header';
-// import Login from './component/common/header/Login';
-// import Signup from './component/common/header/Signup';
+import Login from './component/common/header/Login';
+import Signup from './component/common/header/Signup';
 import Home from './component/home/Home';
 import Team from './component/pages/team/Team';
 import Footer from './component/common/footer/Footer';
 import ContactUs from './component/pages/contactus/ContactUs';
-// import LoginPage from './component/pages/Login/LoginPage';
-// import RegisterPage from './component/pages/Register/RegisterPage';
+import Career from './component/pages/career/Career';
+
+
 
 function App() {
   return (
@@ -23,10 +24,11 @@ function App() {
         <Header />
         <Switch>
           <Route path="/" exact component={Home} />
-          {/* <Route path="/login" exact component={LoginPage} />
-          <Route path="/signup" exact component={RegisterPage} /> */}
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={Signup} />
           <Route path="/team" exact component={Team} />
           <Route path="/contactus" exact component={ContactUs} />
+          <Route path="/career" exact component={Career} />
           {/* <Route path="/faq" exact component={Faq} /> */}
         </Switch>
         <Footer />
