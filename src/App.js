36221ea0214a1e37abc00b381bbe346033555164
jathuRSHAN <sjathurshan19980419@gmail.com>
2025-03-20 12:22:03 +0530

    login

--- src/App.js
+++ src/App.js
@@ -2,29 +2,38 @@
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
+
   //Link
 } from "react-router-dom";
 import Header from './component/common/header/Header';
-import Login from './component/common/header/Login';
-import Signup from './component/common/header/Signup';
+// import Login from './component/common/header/Login';
+// import Signup from './component/common/header/Signup';
 import Home from './component/home/Home';
 import Team from './component/pages/team/Team';
 import Footer from './component/common/footer/Footer';
+import LoginPage from './component/pages/Login/LoginPage';
+import RegisterPage from './component/pages/Register/RegisterPage';
+
 
 function App() {
+
+
   return (
     <>
       <Router>
         <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/signup" exact component={Signup} />
-          <Route path="/team" exact component={Team} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<RegisterPage />} />
+          {/* <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/signup" exact component={RegisterPage} />
+          <Route path="/team" exact component={Team} /> */}
           {/* <Route path="/faq" exact component={Faq} /> */}
-        </Switch>
+        </Routes>
         <Footer />
       </Router>
 
--- src/component/common/header/Header.jsx
+++ src/component/common/header/Header.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from "react"
 import './header.css';
 import { menu } from '../../data';
 import DropdownMenu from './DropdownMenu';
 // import SearchComponent from './SearchComponent';
-import { Link } from "react-router-dom"
+import { Link, useNavigate, } from "react-router-dom"
 import DarkMode from './darkmode/DarkMode';
+import { isAuthenticated } from '../../services/Auth';
+import { logout } from '../../services/Auth';
+import { UserDetailsApi } from "../../services/Api";
 
 export default function Header() {
     // const sampleData = ["Apple", "Banana", "Orange", "Grape", "Pineapple", "Mango"];
@@ -12,6 +15,27 @@ export default function Header() {
     const toggleMenu = () => {
         setclik(!clik);
     }
+
+    const navigate = useNavigate();
+    const logoutUser = () => {
+        logout();
+        navigate('/login')
+    }
+
+    const [user, setUser] = useState({ name: "", email: "", localId: "" })
+    useEffect(() => {
+        if (isAuthenticated()) {
+            UserDetailsApi().then((response) => {
+
+                setUser({
+                    name: response.data.users[0].displayName,
+                    email: response.data.users[0].email,
+                    localId: response.data.users[0].localId,
+                })
+            })
+        }
+    }, [])
+
     return (
         <>
             <header >
@@ -40,8 +64,10 @@ export default function Header() {
                             </div>
                             <div className="button d_flex">
                                 <div className="theme"><DarkMode /></div>
-                                <button className="lonin"><a href="">login</a></button>
-                                <button className="signin"><Link to="">Signup</Link></button>
+                                {!isAuthenticated() ? <button className="lonin"><Link to="login">Login</Link></button> : null}
+                                {!isAuthenticated() ? <button className="signin"><Link to="signup">Signup</Link></button> : null}
+                                {isAuthenticated() ? <h3>{user.name}</h3> : null}
+                                {isAuthenticated() ? <button className="logout" onClick={logoutUser} >logout</button> : null}
                                 {/* <button className="lonin"><a href="/login">login</a></button>
                                 <button className="signin"><Link to="./Signup">Signup</Link></button> */}
                             </div>
